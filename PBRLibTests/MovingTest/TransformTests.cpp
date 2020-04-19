7c1675bb37908e08b9a6232a3e2f8d7f206142d9
Asif Mamedov <asif.mamedov.as98@gmail.com>
2020-04-19 12:14:30 +0700

    Добавил класс Transform и тесты к этому классу.

---
+++ PBRLibTests/MovingTest/TransformTests.cpp
@@ -0,0 +1,176 @@
+//
+//  TransformTests.cpp
+//  PBRLib
+//
+//  Created by Асиф Мамедов on 17/04/2020.
+//  Copyright © 2020 Асиф Мамедов. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+
+#include "../../src/Moving/Transform.hpp"
+#include "../../src/math/vec3.hpp"
+
+using namespace testing;
+using namespace pbrlib;
+
+TEST(MovingTransform, Constructor)
+{
+    constexpr Matrix4x4<float> tm1 (
+        1.0f, 0.0f, 0.0f, 0.0f, 
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    constexpr Matrix4x4<float> tm2 (
+        1.0f, 2.0f, 3.0f, 4.0f,
+        4.0f, 3.0f, 2.0f, 1.0f,
+        5.0f, 6.0f, 7.0f, 8.0f,
+        8.0f, 7.0f, 6.0f, 5.0f
+    );
+
+    Transform t1;
+    Transform t2 (tm2);
+
+    EXPECT_EQ(tm1, t1.getMatrix());
+    EXPECT_EQ(tm2, t2.getMatrix());
+}
+
+TEST(MovingTransform, EqualAndNotEqual)
+{
+    constexpr Matrix4x4<float> tm (
+        1.0f, 2.0f, 3.0f, 4.0f,
+        4.0f, 3.0f, 2.0f, 1.0f,
+        5.0f, 6.0f, 7.0f, 8.0f,
+        8.0f, 7.0f, 6.0f, 5.0f
+    );
+
+    Transform t1;
+    Transform t2;
+
+    EXPECT_TRUE(t1 == t2);
+
+    t2.setMatrix(tm);
+
+    EXPECT_TRUE(t1 != t2);
+}
+
+TEST(MovingTransform, GetMatrixAndInverseMatrix)
+{
+    constexpr Matrix4x4<float> tm (
+        1.0f, 2.0f, 3.0f, 4.0f,
+        4.0f, 3.0f, 2.0f, 1.0f,
+        5.0f, 6.0f, 7.0f, 8.0f,
+        8.0f, 7.0f, 6.0f, 5.0f
+    );
+
+    Transform t (tm);
+
+    EXPECT_EQ(tm, t.getMatrix());
+    EXPECT_EQ(inverse(tm), t.getInverseMatrix());
+}
+
+TEST(MovingTransform, Translate)
+{
+    constexpr Vec3<float> t (0.5f, 0.5f, 0.5f);
+    
+    Transform transform = Transform::translate(t);
+
+    constexpr Matrix4x4<float> r (
+        0.0f, 0.0f, 0.0f, 0.5f,
+        0.0f, 0.0f, 0.0f, 0.5f,
+        0.0f, 0.0f, 0.0f, 0.5f, 
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    EXPECT_EQ(r, transform.getMatrix());
+}
+
+TEST(MovingTransform, Scale)
+{
+    constexpr Vec3<float> t (0.5f, 0.5f, 0.5f);
+    
+    Transform transform = Transform::scale(t);
+
+    constexpr Matrix4x4<float> r (
+        0.5f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.5f, 0.0f, 
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    EXPECT_EQ(r, transform.getMatrix());
+}
+
+TEST(MovingTransform, Rotates)
+{
+    constexpr Vec3<float> r1 (0.4767313f, -0.291967213f, 0.82914561f);
+    constexpr Vec3<float> r2 (0.816371619f, 0.572077572f, -0.079149574f);
+    constexpr Vec3<float> r3 (-0.257068098f, 0.698900223f, 0.667423784f);
+    
+    constexpr float         theta   (60.0f);
+    constexpr Vec3<float>   axis1   (1.0f, 0.0f, 0.0f);
+    constexpr Vec3<float>   axis2   (0.0f, 1.0f, 0.0f);
+    constexpr Vec3<float>   axis3   (0.0f, 0.0f, 1.0f);
+    constexpr Vec3<float>   v       (0.4767313f, 0.572077572f, 0.667423784f);
+
+    Transform rx    = Transform::rotateX(theta);
+    Transform ry    = Transform::rotateY(theta);
+    Transform rz    = Transform::rotateZ(theta);
+    Transform ra1   = Transform::rotate(axis1, theta);
+    Transform ra2   = Transform::rotate(axis2, theta);
+    Transform ra3   = Transform::rotate(axis3, theta);
+
+    Vec3<float> res1 = rx(v);
+    Vec3<float> res2 = ry(v);
+    Vec3<float> res3 = rz(v);
+    Vec3<float> res4 = ra1(v);
+    Vec3<float> res5 = ra2(v);
+    Vec3<float> res6 = ra3(v);
+
+    EXPECT_EQ(r1, res1);
+    EXPECT_EQ(r2, res2);
+    EXPECT_EQ(r3, res3);
+
+    EXPECT_EQ(r1, res4);
+    EXPECT_EQ(r2, res5);
+    EXPECT_EQ(r3, res6);
+}
+
+TEST(MovingTransform, LookAt)
+{
+    constexpr Matrix4x4<float> res (
+        0.0995037183f,  -0.663813591f,  0.741249323f,   0.0f,
+        0.0f,           0.744946361f,   0.667124391f,   0.0f,
+        -0.995037198f,  -0.0663813576f, 0.0741249323f,  0.0f,
+        -0.497518539f,  -3.38544893f,   -63.673317f,    1.0f
+    );
+
+    constexpr Vec3<float> pos   (45.0f, 45.0f, 4.0f);
+    constexpr Vec3<float> eye   (-5.0f, 0.0f, -1.0f);
+    constexpr Vec3<float> up    (0.0f, 1.0f, 0.0f);
+
+    Transform look_at = Transform::lookAt(pos, eye, up);
+    
+    EXPECT_EQ(res, look_at.getMatrix());
+}
+
+TEST(MovingTransform, PrespectiveProjection)
+{
+    constexpr Matrix4x4<float> res (
+        -0.117089964f,  0.0f,           0.0f,           0.0f,
+        0.0f,           -0.156119958f,  0.0f,           0.0f,
+        0.0f,           0.0f,           -1.0000999f,    -1.0f,
+        0.0f,           0.0f,           -0.0200009998f, 0.0f
+    );
+
+    constexpr float fov       = 60.0f;
+    constexpr float aspect    = 4.0f / 3.0f;
+    constexpr float z_near    = 0.01f;
+    constexpr float z_far     = 200.0f;
+
+    Transform perspective_projection = Transform::perspective(fov, aspect, z_near, z_far);
+
+    EXPECT_EQ(res, perspective_projection.getMatrix());
+}
---
+++ src/Moving/Transform.cpp
@@ -0,0 +1,225 @@
+//
+//  Transform.cpp
+//  PBRLib
+//
+//  Created by Асиф Мамедов on 17/04/2020.
+//  Copyright © 2020 Асиф Мамедов. All rights reserved.
+//
+
+#include "Transform.hpp"
+
+#include "../math/matrix4x4.hpp"
+
+#include <cmath>
+
+namespace pbrlib
+{
+    float getRadians(float angle)
+    {
+        constexpr float p = M_PI / 180.0f;
+        return angle * p;
+    }
+
+    /*  -------------------------   */
+    constexpr Matrix4x4<float> Transform::IDENTITY_MATRIX;
+
+    Transform::Transform()
+    {}
+
+    Transform::Transform(const Matrix4x4<float>& m) :
+        _m(m)
+    {}
+
+    bool Transform::operator == (const Transform& t) const
+    {
+        return _m == t._m;
+    }
+
+    bool Transform::operator != (const Transform& t) const
+    {
+        return _m != t._m;
+    }
+
+    Vec3<float> Transform::operator () (const Vec3<float>& v) const
+    {
+        return {
+            _m[0][0] * v.x + _m[0][1] * v.y + _m[0][2] * v.z,
+            _m[1][0] * v.x + _m[1][1] * v.y + _m[1][2] * v.z,
+            _m[2][0] * v.x + _m[2][1] * v.y + _m[2][2] * v.z
+        };
+    }
+
+    Transform Transform::operator * (const Transform& t) const
+    {
+        return Transform(_m * t._m);
+    }
+
+    bool Transform::identity() const
+    {
+        return _m == IDENTITY_MATRIX;
+    }
+
+    Matrix4x4<float>& Transform::getMatrix() noexcept
+    {
+        return _m;
+    }
+
+    const Matrix4x4<float>& Transform::getMatrix() const noexcept
+    {
+        return _m;
+    }
+
+    Matrix4x4<float> Transform::getInverseMatrix() const noexcept
+    {
+        return inverse(_m);
+    }
+
+    void Transform::setMatrix(const Matrix4x4<float>& m)
+    {
+        _m = m;
+    }
+
+    Transform inverse(const Transform& t)
+    {
+        return Transform(inverse(t._m));
+    }
+
+    Transform transpose(const Transform& t)
+    {
+        return Transform(transpose(t._m));
+    }
+
+    Transform Transform::translate(const Vec3<float>& t)
+    {
+        return Transform(Matrix4x4<float>(
+            0.0f, 0.0f, 0.0f, t.x,
+            0.0f, 0.0f, 0.0f, t.y,
+            0.0f, 0.0f, 0.0f, t.z,
+            0.0f, 0.0f, 0.0f, 1.0f
+        ));
+    }
+
+    Transform Transform::scale(const Vec3<float>& s)
+    {
+        return Transform(Matrix4x4<float>(
+            s.x,  0.0f, 0.0f, 0.0f,
+            0.0f, s.y,  0.0f, 0.0f,
+            0.0f, 0.0f, s.z,  0.0f,
+            0.0f, 0.0f, 0.0f, 1.0f
+        ));
+    }
+
+    Transform Transform::rotateX(float theta)
+    {
+        float sin_theta = sin(getRadians(theta));
+        float cos_theta = cos(getRadians(theta));
+
+        return Transform(Matrix4x4<float>(
+            1.0f, 0.0f,         0.0f,       0.0f,
+            0.0f, cos_theta,    -sin_theta, 0.0f,
+            0.0f, sin_theta,    cos_theta,  0.0f,
+            0.0f, 0.0f,         0.0f,       1.0f
+        ));
+    }
+
+    Transform Transform::rotateY(float theta)
+    {
+        float sin_theta = sin(getRadians(theta));
+        float cos_theta = cos(getRadians(theta));
+
+        return Transform(Matrix4x4<float>(
+            cos_theta,  0.0f, sin_theta,    0.0f,
+            0.0f,       1.0f, 0.0f,         0.0f,
+            -sin_theta, 0.0f, cos_theta,    0.0f,
+            0.0f,       0.0f, 0.0f,         1.0f
+        ));
+    }
+
+    Transform Transform::rotateZ(float theta)
+    {
+        float sin_theta = sin(getRadians(theta));
+        float cos_theta = cos(getRadians(theta));
+
+        return Transform(Matrix4x4<float>(
+            cos_theta,  -sin_theta, 0.0f, 0.0f,
+            sin_theta,  cos_theta,  0.0f, 0.0f,
+            0.0f,       0.0f,       1.0f, 0.0f,
+            0.0f,       0.0f,       0.0f, 1.0f
+        ));
+    }
+
+    Transform Transform::rotate(const Vec3<float>& axis, float theta)
+    {
+        Vec3<float> r = normalize(axis);
+
+        float theta_in_radians = getRadians(theta);
+
+        float sin_theta             = sin(theta_in_radians);
+        float cos_theta             = cos(theta_in_radians);
+        float one_minus_cos_theta   = 1.0f - cos_theta;
+
+        float rxry = r.x * r.y;
+        float rxrz = r.x * r.z;
+        float ryrz = r.y * r.z;
+
+        float m11 = r.x * r.x * one_minus_cos_theta + cos_theta;
+        float m12 = rxry * one_minus_cos_theta - r.z * sin_theta;
+        float m13 = rxrz * one_minus_cos_theta + r.y * sin_theta;
+
+        float m21 = rxry * one_minus_cos_theta + r.z * sin_theta;
+        float m22 = r.y * r.y * one_minus_cos_theta + cos_theta;
+        float m23 = ryrz * one_minus_cos_theta - r.x * sin_theta;
+
+        float m31 = rxrz * one_minus_cos_theta - r.y * sin_theta;
+        float m32 = ryrz * one_minus_cos_theta + r.x * sin_theta;
+        float m33 = r.z * r.z * one_minus_cos_theta + cos_theta;
+
+        return Transform(Matrix4x4<float>(
+            m11,    m12,    m13,    0.0f,
+            m21,    m22,    m23,    0.0f,
+            m31,    m32,    m33,    0.0f,
+            0.0f,   0.0f,   0.0f,   1.0f
+        ));
+    }
+
+    Transform Transform::lookAt(
+        const Vec3<float>& pos, 
+        const Vec3<float>& eye, 
+        const Vec3<float>& up
+    )
+    {
+        Vec3<float> d = normalize(pos - eye);
+        Vec3<float> r = normalize(cross(up, d));
+        Vec3<float> u = cross(d, r);
+
+        return Transform(Matrix4x4<float>(
+            r.x,            u.x,            d.x,            0.0f,
+            r.y,            u.y,            d.y,            0.0f,
+            r.z,            u.z,            d.z,            0.0f,
+            -dot(r, pos),   -dot(u, pos),   -dot(d, pos),   1.0f
+        ));
+    }
+
+    Transform Transform::perspective(
+        float fovy, 
+        float aspect, 
+        float z_near, 
+        float z_far
+    )
+    {
+        float a = tan(fovy / 2.0f);
+
+        return Transform(Matrix4x4<float>(
+            1.0f / (aspect * a),    0.0f,       0.0f,                                           0.0f,
+            0.0f,                   1.0f / a,   0.0f,                                           0.0f, 
+            0.0f,                   0.0f,       -(z_far + z_near) / (z_far - z_near),           -1.0f,
+            0.0f,                   0.0f,       -(2.0f * z_far * z_near) / (z_far - z_near),    0.0f
+        ));
+    }
+
+    ostream& operator << (ostream& print, const Transform& t)
+    {
+        print << t._m;
+        return print;
+    }
+}
