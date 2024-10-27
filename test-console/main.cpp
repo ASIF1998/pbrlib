9983856f787d596ccef32aca56c5c5e101028654
ASIF1998 <asif.mamedov.98@bk.ru>
2024-10-27 21:03:22 +0300

    Изменил test-console/main.cpp, в котором пока будет отражаться то, что я примерно хочу видеть от api дижка.

--- test-console/main.cpp
+++ test-console/main.cpp
@@ -1,225 +1,81 @@
-#include <pbrlib/Rendering/Window.hpp>
-#include <pbrlib/PBRLibResources.hpp>
-#include <pbrlib/scene/scene_view.hpp>
+#include <pbrlib/engine.hpp>
+#include <pbrlib/config.hpp>
+
+#include <pbrlib/moving/transform.hpp>
+
 #include <pbrlib/scene/scene.hpp>
+#include <pbrlib/scene/component.hpp>
 
+#include <pbrlib/Rendering/Lights/PointLight.hpp>
 #include <pbrlib/Rendering/Cameras/PerspectiveCamera.hpp>
-
-#include <pbrlib/Rendering/Geometry/Mesh.hpp>
-#include <pbrlib/scene/mesh_node.hpp>
-
-#include <pbrlib/Rendering/Renderers/PBR.hpp>
-
-#include <pbrlib/input/input_stay.hpp>
 
 #include <pbrlib/logger/logger.hpp>
 
-#include <iostream>
-#include <thread>
+#include <pbrlib/input/input_stay.hpp>
 
-#include <filesystem>
-
-using namespace std;
-using namespace pbrlib;
-using namespace pbrlib::math;
-
-const static std::filesystem::path mediatestsRoot = "C:\\Dev\\media";
-
-struct ModelResourcesReferences
+class RotateComponent :
+    public pbrlib::Component<RotateComponent>
 {
-	std::filesystem::path path_to_mesh;
-	std::filesystem::path path_to_albedo;
-	std::filesystem::path path_to_normal_map;
-	std::filesystem::path path_to_metallic;
-	std::filesystem::path path_to_roughness;
-	std::filesystem::path path_to_backed_ao;
-};
+    void update(const pbrlib::InputStay* ptr_input_stay, pbrlib::SceneItem* ptr_node, float delta_time) override
+    {
+        if (ptr_input_stay->keyboard.isDown(pbrlib::Keycode::Enter)) 
+            _rotate_angle += 0.1f;
+    }
 
-class Rotate: 
-    public Component<Rotate>
-{
 public:
-    Rotate() 
-    {
-        _frame = 0;
-    }
-
-    void update(const InputStay* ptr_input_stay, SceneItem* ptr_node, float delta_time) override
-    {
-		_rotate = Transform::rotateY(static_cast<float>(++_frame));
-		ptr_node->getLocalTransform() = Transform::scale(0.7f) * _rotate;
-    }
-
-    static auto make() 
-    {
-        return std::make_shared<Rotate>();
-    }
+    RotateComponent() :
+        Component("rotate-compnent")
+    { }
 
 private:
-    Transform _rotate;
-    size_t _frame;
+    float _rotate_angle = 0.0f;
 };
 
-class CameraController
-    : public Component<CameraController>
+int main()
 {
-public:
-    CameraController() = default;
-
-    void update(const InputStay* ptr_input_stay, SceneItem* ptr_node, float delta_time) override
+    try 
     {
-        auto& camera = ptr_node->getComponent<CameraBase>();
-
-        constexpr Vec3<float> eye (0, 1, 0);
-        constexpr Vec3<float> up (0, 1, 0);
-
-        auto pos = camera.getPosition();
-
-        if (ptr_input_stay->keyboard.isDown(Keycode::W))
-            ++pos.z;
-
-        if (ptr_input_stay->keyboard.isDown(Keycode::S))
-            --pos.z;
-
-        if (ptr_input_stay->keyboard.isDown(Keycode::Space))
-            ++pos.y;
-
-        if (ptr_input_stay->keyboard.isDown(Keycode::ShiftLeft))
-            --pos.y;
-
-        if (ptr_input_stay->keyboard.isDown(Keycode::A))
-            --pos.x;
+        pbrlib::Config config;
+        config.title = "test-console";
         
-        if (ptr_input_stay->keyboard.isDown(Keycode::D))
-            ++pos.x;
-            
-        camera.setLookAt(eye, pos, up);
+        pbrlib::Engine engine (config);
+
+        /// @todo Написать ещё часть, где будет работа с компанентами.
+
+        engine.setupCallback([](const pbrlib::Engine* ptr_engine, pbrlib::Scene* ptr_scene)
+        {
+            const auto filename = pbrlib::path_to_root / "pbrlib-tests/content/Blender 2.glb";
+            pbrlib::log::info("Path to content: {}", filename.string());
+
+            // ptr_scene->addModel(filename, pbrlib::Transform::translate(pbrlib::math::vec3(0, 0, 0)));
+
+            // auto ptr_light = ptr_scene->addLight(
+            //     pbrlib::PointLight::Builder()
+            //         .setPosition(pbrlib::math::vec3(0, 10, 10))
+            //         .setName("point-light")
+            //         .setIntensity(100.0)
+            //         .setColor(pbrlib::math::vec3(0.32, 0.43, 0.12))
+            //         .build()
+            // );
+
+            // auto ptr_camera = ptr_engine->camera(
+            //     pbrlib::Camera::Builder()
+            //         .setAspect(800.0 / 600)
+            //         .setEye(pbrlib::math::vec3(0, 0, 0))
+            //         .setPosition(pbrlib::math::vec3(0, 0, -10))
+            //         .setUp(pbrlib::math::vec3(0, 1, 0))
+            //         .setFar(100.0)
+            //         .setNear(0.01)
+            //         .setViewport(0, 0, 800.0f, 600.0f, 0.01, 100.0f)
+            //         .build()
+            // );
+        });
+
+        engine.run();
     }
-};
-
-auto getModelResourcesReferences() 
-    -> ModelResourcesReferences
-{
-    ModelResourcesReferences refs;
-
-    refs.path_to_mesh 			= mediatestsRoot / "pbr-sphere/source/sphere_sampleScene_1cm.fbx";
-    refs.path_to_albedo 		= mediatestsRoot / "pbr-sphere/textures/sphere_albedo.jpg";
-    refs.path_to_normal_map 	= mediatestsRoot / "pbr-sphere/textures/sphere_normal.jpg";
-    refs.path_to_metallic 		= mediatestsRoot / "pbr-sphere/textures/sphere_metallic.jpg";
-    refs.path_to_roughness 		= mediatestsRoot / "pbr-sphere/textures/sphere_roughness.jpg";
-    refs.path_to_backed_ao 		= mediatestsRoot / "pbr-sphere/textures/sphere_AO.jpg";
-
-    return refs;
-}
-
-std::shared_ptr<Window> makeWindow()
-{
-    Window::Builder window_builder;
-    window_builder.setTitle("Sand Box");
-    window_builder.setExtend(800, 600);
-    window_builder.setPosition(Window::WINDOW_POSITION_CENTERED, Window::WINDOW_POSITION_CENTERED);
-    window_builder.setResizableWindow(Window::Resizable::STATIC);
-
-    return window_builder.buildPtr();
-}
-
-auto getMaterial(const ModelResourcesReferences& model, GPUTextureManager& texture_manager, MaterialManager& material_manager)
-{
-    Material::Builder material_builder (texture_manager);
-    material_builder.setAlbedo(model.path_to_albedo.string(), "Albedo");
-    material_builder.setBakedAO(model.path_to_backed_ao.string(), "AO");
-    material_builder.setMetallic(model.path_to_metallic.string(), "Mettalic");
-    material_builder.setNormalMap(model.path_to_normal_map.string(), "Normal map");
-    material_builder.setRoughness(model.path_to_roughness.string(), "Roughness");
-    material_builder.setAnisotropy(0.3f);
-
-    std::shared_ptr material = material_builder.buildPtr();
-    material_manager.addMaterial(material, "Material");
-
-    return material;
-}
-
-auto makeCamera(Scene& scene)
-{
-    PerspectiveCamera::Builder camera_builder;
-    camera_builder.setName("Camera");
-    camera_builder.setAspect(800, 600);
-    camera_builder.setEye({-0.9f, 0.5f, 0.5f});
-    camera_builder.setPosition({0, 0, -2.0});
-    camera_builder.setFarClipp(10.0f);
-    camera_builder.setFovy(60.78f);
-    camera_builder.setNearClipp(0.01f);
-    camera_builder.setUp({0, 1, 0});
-    camera_builder.setViewport(0, 0, 800, 600, 0.01f, 2000.0f);
-
-    auto ptr_camera_node = scene.makeCamera(camera_builder);
-
-    return ptr_camera_node;
-}
-
-auto makePointLight(Scene& scene)
-{
-    PointLight::Builder light_builder;
-    light_builder.setName("Point Light");
-    light_builder.setColor({1, 1, 1});
-    light_builder.setIntensity(10.0f);
-    light_builder.setPosition({0, 0, 5});
-
-    return scene.makePointLight(light_builder);
-}
-
-int main(int argc, char* argv[])  
-{
-    try
+    catch (const std::exception& ex)
     {
-        std::shared_ptr engine_resources = PBRLibResources::init();
-
-        log::info("Demo scene.");
-
-        auto window = makeWindow(); 
-
-        SceneView scene_view ("Scene", engine_resources, window);
-
-        auto model = getModelResourcesReferences();
-        std::shared_ptr<SceneItem> ptr_point_light = nullptr;
-
-        auto& scene               = scene_view.getScene();
-        auto& mesh_manager        = scene.getMeshManager();
-        auto& material_manager    = scene.getMaterialManager();
-        auto& texture_manager     = scene.getTextureManager();
-
-        auto model_meshes   = mesh_manager.load(model.path_to_mesh.string()).value();
-        auto material       = getMaterial(model, texture_manager, material_manager);
-        model_meshes[0]->setMaterial(material);
-
-        std::shared_ptr mesh_node = MeshNode::make("Floor", model_meshes[0]);
-        mesh_node->addComponent(Rotate::make());
-
-        // Регестрируем модель в сцене.
-        scene.setRoot(mesh_node);
-
-        auto ptr_camera = makeCamera(scene);
-        ptr_camera->addComponent(std::make_shared<CameraController>());
-        
-        auto light    = makePointLight(scene);
-
-        PBRPass::Optionals pbr_optionals;
-        pbr_optionals.setDistributionFunction(PBRPass::DistributionFunction::GGX);
-        pbr_optionals.setGeometryFunction(PBRPass::GeometryFunction::Kelemen);
-        pbr_optionals.setFresnelApproximation(PBRPass::FresnelApproximation::Schlick);
-
-        std::shared_ptr renderer = PBR::make(pbr_optionals);
-        scene_view.setRenderer(renderer);
-
-        while (scene_view.drawScene(1323));
-    } 
-    catch (std::exception& ex)
-    {
-        std::cerr << ex.what() << std::endl;
-    }
-    catch (...)
-    {
-        std::cerr << "error" << std::endl;
+        pbrlib::log::error(ex.what());
     }
 
     return 0;
