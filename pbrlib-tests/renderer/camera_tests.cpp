c5583dee9a165265930dcc54a02e4d1d07f2d1ab
ASIF1998 <asif.mamedov.98@bk.ru>
2024-10-29 08:50:44 +0300

    Убрал лишние тесты.

---
+++ pbrlib-tests/renderer/camera_tests.cpp
@@ -0,0 +1,26 @@
+#include "../utils.hpp"
+
+TEST(CameraTests, Ctor)
+{
+    /// @todo
+}
+
+TEST(CameraTests, Setters)
+{
+    /// @todo
+}
+
+TEST(CameraTests, Getters)
+{
+    /// @todo
+}
+
+TEST(CameraTests, ViewMatrix)
+{
+/// @todo
+}
+
+TEST(CameraTests, ProjectionMatrix)
+{
+    /// @todo
+}
