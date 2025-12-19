b84962347eddf0a6176241d3c9f30936b490e744
ASIF1998 <asif.mamedov.98@bk.ru>
2025-12-19 22:06:26 +0300

    delete unnecessary export.glsl files from blur

--- backend/shaders/bilateral_blur/exports.glsl
+++
@@ -1,6 +0,0 @@
-#ifndef PBRLIB_BILATERAL_BLUR_EXPORTS_GLSL
-#define PBRLIB_BILATERAL_BLUR_EXPORTS_GLSL
-
-layout(set = PBRLIB_BILATERAL_BLUR_EXPORTS_SET_ID, binding = 0) uniform sampler2D bilateral_blur_result;
-
-#endif
